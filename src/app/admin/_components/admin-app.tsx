82db7a3b3ce34ba1206bdee6105e41fc4806994d
Elijah Fasugba <elijahfasugba@gmail.com>
2025-11-09 09:41:06 +0000

    Let this align to top instead of starting below the page also make sure

--- src/app/admin/_components/admin-app.tsx
+++ src/app/admin/_components/admin-app.tsx
@@ -286,7 +286,7 @@ export default function AdminApp({
           <Sidebar>
              <SidebarMenuContent />
           </Sidebar>
-          <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-60">
+          <div className="flex flex-col sm:gap-4 sm:pl-60">
             <AdminHeader />
             <main className="flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
               {children}
