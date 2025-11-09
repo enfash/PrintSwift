c2ca0514a668e326e78ca4cfb88a8e6a992f372c
Elijah Fasugba <elijahfasugba@gmail.com>
2025-11-09 02:09:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2356612765-c90c9",
+  "appId": "1:375777032627:web:df3f87378e89847ae1f3c6",
+  "apiKey": "AIzaSyCaDBzuSLrIE8yJPwFIEhKkZ5os-1nKIXg",
+  "authDomain": "studio-2356612765-c90c9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "375777032627"
+};
