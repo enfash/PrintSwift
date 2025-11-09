5e90712f87ec357990a50e67e491232a79def05a
Elijah Fasugba <elijahfasugba@gmail.com>
2025-11-09 09:10:18 +0000

    You haven't remove the header <header class="sticky top-0 z-50 w-full b

--- src/app/admin/_components/admin-app.tsx
+++ src/app/admin/_components/admin-app.tsx
@@ -175,7 +175,7 @@ function AdminHeader() {
   };
   
   return (
-    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+    <section>
        <SidebarTrigger className="sm:hidden"/>
       <div className="relative ml-auto flex-1 md:grow-0">
         <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
@@ -209,7 +209,7 @@ function AdminHeader() {
           <DropdownMenuItem onClick={handleLogout}>Logout</DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
-    </header>
+   </section>
   );
 }
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 
+'use client';
 import type { Metadata } from 'next';
 import './globals.css';
 import { Header } from '@/components/layout/header';
@@ -6,17 +7,21 @@ import { Footer } from '@/components/layout/footer';
 import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
 import { FirebaseClientProvider } from '@/firebase';
-
-export const metadata: Metadata = {
-  title: 'PrintSwift',
-  description: 'Affordable Custom Branding, Delivered Fast.',
-};
+import { usePathname } from 'next/navigation';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+  const isAdminPage = pathname.startsWith('/admin');
+
+  const metadata: Metadata = {
+    title: 'PrintSwift',
+    description: 'Affordable Custom Branding, Delivered Fast.',
+  };
+
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
@@ -28,9 +33,9 @@ export default function RootLayout({
       <body className={cn('font-body antialiased')}>
           <FirebaseClientProvider>
             <div className="relative flex min-h-screen flex-col bg-background">
-              <Header />
+              {!isAdminPage && <Header />}
               <main className="flex-1">{children}</main>
-              <Footer />
+              {!isAdminPage && <Footer />}
               <Toaster />
             </div>
           </FirebaseClientProvider>
