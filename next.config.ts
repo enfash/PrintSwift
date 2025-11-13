533c2d97673d8227f5876fde15a14acc8668612e
Elijah Fasugba <elijahfasugba@gmail.com>
2025-11-13 17:03:24 +0000

    image:1  GET https://6000-firebase-studio-1762651024713.cluster-64pjnskm

--- next.config.ts
+++ next.config.ts
@@ -23,31 +23,11 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      }
+    domains: [
+        'placehold.co',
+        'images.unsplash.com',
+        'picsum.photos',
+        'firebasestorage.googleapis.com',
     ],
   },
 };
