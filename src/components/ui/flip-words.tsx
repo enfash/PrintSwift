29c5242f213e645f9e6913f10e59b0dd5170d157
Elijah Fasugba <elijahfasugba@gmail.com>
2025-11-23 12:21:49 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 'mo

--- src/components/ui/flip-words.tsx
+++ src/components/ui/flip-words.tsx
@@ -1,6 +1,6 @@
 'use client';
 import * as React from 'react';
-import { AnimatePresence, motion } from 'motion/react';
+import { AnimatePresence, motion } from 'framer-motion';
 import { cn } from '@/lib/utils';
 
 type FlipWordsProps = {
